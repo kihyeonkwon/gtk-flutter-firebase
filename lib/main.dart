41c25cc67d3e2a1ec50bbde2be2c7f6a545d0883
Kihyeon Kwon <kihyeon.kwn@gmail.com>
2022-12-12 15:11:34 +0900

    write message to firestore

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'src/authentication.dart';
 import 'firebase_options.dart';
@@ -49,34 +50,37 @@ class App extends StatelessWidget {
           return const HomePage();
         },
         '/sign-in': ((context) {
-          return SignInScreen(
-            actions: [
-              ForgotPasswordAction(((context, email) {
-                Navigator.of(context)
-                    .pushNamed('/forgot-password', arguments: {'email': email});
-              })),
-              AuthStateChangeAction(((context, state) {
-                if (state is SignedIn || state is UserCreated) {
-                  var user = (state is SignedIn)
-                      ? state.user
-                      : (state as UserCreated).credential.user;
-                  if (user == null) {
-                    return;
+          return Scaffold(
+            appBar: AppBar(),
+            body: SignInScreen(
+              actions: [
+                ForgotPasswordAction(((context, email) {
+                  Navigator.of(context).pushNamed('/forgot-password',
+                      arguments: {'email': email});
+                })),
+                AuthStateChangeAction(((context, state) {
+                  if (state is SignedIn || state is UserCreated) {
+                    var user = (state is SignedIn)
+                        ? state.user
+                        : (state as UserCreated).credential.user;
+                    if (user == null) {
+                      return;
+                    }
+                    if (state is UserCreated) {
+                      user.updateDisplayName(user.email!.split('@')[0]);
+                    }
+                    if (!user.emailVerified) {
+                      user.sendEmailVerification();
+                      const snackBar = SnackBar(
+                          content: Text(
+                              'Please check your email to verify your email address'));
+                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                    }
+                    Navigator.of(context).pushReplacementNamed('/home');
                   }
-                  if (state is UserCreated) {
-                    user.updateDisplayName(user.email!.split('@')[0]);
-                  }
-                  if (!user.emailVerified) {
-                    user.sendEmailVerification();
-                    const snackBar = SnackBar(
-                        content: Text(
-                            'Please check your email to verify your email address'));
-                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                  }
-                  Navigator.of(context).pushReplacementNamed('/home');
-                }
-              })),
-            ],
+                })),
+              ],
+            ),
           );
         }),
         '/forgot-password': ((context) {
@@ -113,6 +117,10 @@ class ApplicationState extends ChangeNotifier {
   bool _loggedIn = false;
   bool get loggedIn => _loggedIn;
 
+  StreamSubscription<QuerySnapshot>? _guestBookSubscription;
+  List<GuestBookMessage> _guestBookMessages = [];
+  List<GuestBookMessage> get guestBookMessages => _guestBookMessages;
+
   Future<void> init() async {
     await Firebase.initializeApp(
         options: DefaultFirebaseOptions.currentPlatform);
@@ -124,10 +132,102 @@ class ApplicationState extends ChangeNotifier {
     FirebaseAuth.instance.userChanges().listen((user) {
       if (user != null) {
         _loggedIn = true;
+        _guestBookSubscription = FirebaseFirestore.instance
+            .collection('guestbook')
+            .orderBy('timestamp', descending: true)
+            .snapshots()
+            .listen((snapshot) {
+          _guestBookMessages = [];
+          for (var document in snapshot.docs) {
+            _guestBookMessages.add(GuestBookMessage(
+                name: document.data()['name'] as String,
+                message: document.data()['text'] as String));
+          }
+        });
       } else {
         _loggedIn = false;
+        _guestBookMessages = [];
+        _guestBookSubscription?.cancel();
       }
       notifyListeners();
     });
   }
+
+  Future<DocumentReference> addMessageToGuestBook(String message) {
+    if (!_loggedIn) {
+      throw Exception('Must be logged in');
+    }
+
+    return FirebaseFirestore.instance
+        .collection('guestbook')
+        .add(<String, dynamic>{
+      'text': message,
+      'timestamp': DateTime.now().microsecondsSinceEpoch,
+      'name': FirebaseAuth.instance.currentUser!.displayName,
+      'userId': FirebaseAuth.instance.currentUser!.uid,
+    });
+  }
+}
+
+class GuestBook extends StatefulWidget {
+  const GuestBook({required this.addMessage, super.key});
+  final FutureOr<void> Function(String message) addMessage;
+
+  @override
+  State<GuestBook> createState() => _GuestBookState();
+}
+
+class _GuestBookState extends State<GuestBook> {
+  final _formKey = GlobalKey<FormState>(debugLabel: '_GuestBookState');
+  final _controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Form(
+        key: _formKey,
+        child: Row(
+          children: [
+            Expanded(
+              child: TextFormField(
+                controller: _controller,
+                decoration: const InputDecoration(hintText: 'Leave a Message'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Enter your Message';
+                  }
+                  return null;
+                },
+              ),
+            ),
+            const SizedBox(
+              width: 8,
+            ),
+            StyledButton(
+                child: Row(children: const [
+                  Icon(Icons.send),
+                  SizedBox(
+                    width: 4,
+                  ),
+                  Text('Send')
+                ]),
+                onPressed: () async {
+                  if (_formKey.currentState!.validate()) {
+                    await widget.addMessage(_controller.text);
+                    _controller.clear();
+                  }
+                })
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class GuestBookMessage {
+  GuestBookMessage({required this.name, required this.message});
+
+  final String name;
+  final String message;
 }
